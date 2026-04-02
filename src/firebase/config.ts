05dfb7ab4483a51919d31a32abf92ff526b36d1d
BOb Maguaer <maguaerbob@gmail.com>
2026-04-02 10:14:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5273470248-b0cf3",
+  "appId": "1:436781181001:web:19d2be4dbc4a0ef9747798",
+  "apiKey": "AIzaSyA0dldL64qbHkgREXuL3JjqHtjkdTiz5ZE",
+  "authDomain": "studio-5273470248-b0cf3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "436781181001"
+};
